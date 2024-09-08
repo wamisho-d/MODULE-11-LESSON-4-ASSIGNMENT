26b1ed72694d864fff800a5d22ce89b392971cac
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-08 12:23:07 -0400

    Update question 2 comics.js

---
+++ question 2 comics.js
@@ -0,0 +1,13 @@
+// src/components/Comics.js
+import React from 'react';
+
+const Comics = () => {
+    return (
+        <div>
+            <h1>Comics</h1>
+            <p>List of Comics...</p>
+        </div>
+    );
+};
+
+export default Comics;
