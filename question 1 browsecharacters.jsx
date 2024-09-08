c63b7c1ae023273616b043e6f9bf0864c33a2426
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-08 12:20:28 -0400

    Update question 1 browsecharacters.jsx

---
+++ question 1 browsecharacters.jsx
@@ -0,0 +1,14 @@
+// src/components/Home.jsx
+import React from 'react';
+
+const BrowseCharacters = () => {
+    return (
+        <div>
+          <h2>Browse Characters</h2>
+          <p>List of characters will be displayed here.</p>
+        </div>
+
+    );
+};
+
+export default BrowseCharacters;
