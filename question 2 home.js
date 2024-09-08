95d456901249ed0b1901ca6b345dd51618cfb3c7
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-08 12:23:29 -0400

    Update question 2 home.js

---
+++ question 2 home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Comic Book Library</h1>
+            <p>Select a page to brows characters or comics.</p>
+        </div>
+
+    );
+};
+
+export default Home;
