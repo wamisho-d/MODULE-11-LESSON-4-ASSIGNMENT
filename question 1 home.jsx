0479e8bff8a5d85ff0910bd6e818037d4b16c417
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-08 12:21:35 -0400

    Update question 1 home.jsx

---
+++ question 1 home.jsx
@@ -0,0 +1,14 @@
+// src/components/Home.jsx
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Welcome to the Comic Book Library!</p>
+        </div>
+
+    );
+};
+
+export default Home;
